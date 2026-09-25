d675472ab29c28cf74fbeb18a97d693a98f34d15
thuybt26 <thuyhy87@gmail.com>
2026-09-25 09:44:26 +0700

    Initial commit: Accessible Interactive Photo Gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,16 +1,16 @@
 function upDate(previewPic) {
-    console.log("Mouse over:", previewPic.src);
+    console.log("Mouse over / Focus:", previewPic.src);
 
     var caption = document.getElementById("caption");
     var image = document.getElementById("image");
 
     caption.innerHTML = previewPic.alt;
-    caption.style.color = "white"; // khi hover thì chữ trắng
+    caption.style.color = "white"; // khi hover/focus thì chữ trắng
     image.style.backgroundImage = "url(" + previewPic.src + ")";
 }
 
 function undo() {
-    console.log("Mouse out");
+    console.log("Mouse out / Blur");
 
     var caption = document.getElementById("caption");
     var image = document.getElementById("image");
@@ -18,4 +18,12 @@ function undo() {
     caption.innerHTML = "Hover over an image below to display here.";
     caption.style.color = "black"; // trở lại màu đen
     image.style.backgroundImage = "url('')";
+}
+
+function initGallery() {
+    console.log("Page loaded – adding tabindex");
+    var thumbnails = document.querySelectorAll(".thumbnails img");
+    for (var i = 0; i < thumbnails.length; i++) {
+        thumbnails[i].setAttribute("tabindex", "0");
+    }
 }
